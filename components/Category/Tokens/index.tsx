f8f26fbe0bce5cc96e2b4f52e55f742b7a024b60
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2022-11-23 12:44:27 +0100

    Update index.tsx

--- components/Category/Tokens/index.tsx
+++ components/Category/Tokens/index.tsx
@@ -1,35 +1,4 @@
-import React, { useState, useEffect } from "react";
-import { Box, Text } from "@chakra-ui/react";
-
-import DexSelectBtn from "../DexSelectBtn";
-import TokenTable from "./TokenTable";
-import DexTicker from "../DexTicker";
-
-//API Key
-const API_KEY = "ckey_4e73d56514984838ab3206fbaf4";
-
-function Tokens() {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    items;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const getApi = async (e: {
-    target: { elements: { chainId: { value: any }; dexName: { value: any } } };
-    preventDefault: () => void;
-  }) => {
-    const chainId = e.target.elements.chainId.value;
-    e.preventDefault();
-    const dexName = e.target.elements.dexName.value;
-    e.preventDefault();
-
-    const response = await fetch(
-      `https://api.covalenthq.com/v1/${chainId}/xy=k/${dexName}/tokens/?quote-currency=USD&format=JSON&key=${API_KEY}`
-    );
-
-    const data = await response.json();
+const data = await response.json();
     setItems(data.data.items);
     console.log(items);
   };
@@ -68,3 +37,8 @@ function Tokens() {
   );
 }
 export default Tokens;
+
+
+  
+    
+    
